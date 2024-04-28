6a26744f97a854bd10e58a3ad1746a222e66afec
Archa Amrita Roy <archaamrita@gmail.com>
2024-04-28 20:11:09 +0200

    changes in bookmark bar css

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,11 +3,11 @@ import Bookmark from '../Bookmark/Bookmark';
 
 const Bookmarks = ({ bookmarks, readingtime }) => {
     return (
-        <div className='md: w-1/3'>
-            <div className='bg-red-100 rounded-md m-6 p-4'>
-                <h2>Reading Time: {readingtime}</h2>
+        <div className='md: w-1/3 bg-red-100 rounded-md ml-4 mt-10'>
+            <div className='bg-red-50 rounded-md m-6 p-4'>
+                <h2 className='text-1xl'>Spend Time on Read: {readingtime}</h2>
             </div>
-            <h1 className='text-3xl bg-red-50 rounded-md m-6 p-4'>Bookmarked Blogs: {bookmarks.length}</h1>
+            <h1 className='text-3xl bg-red-100 rounded-md p-4'>Bookmarked Blogs: {bookmarks.length}</h1>
             {
                 bookmarks.map((bookmark, idx) => <Bookmark 
                     key={idx} 
